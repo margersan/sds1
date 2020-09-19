d836f389c3c88da01218fe7bba6ab9e51daf9d87
margersan <margersan@gmail.com>
2020-09-19 14:44:27 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,110 @@
+package com.devsuperior.dspesquisa.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "tb_game")	//Nome da Tabela no banco de dados
+public class Game implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String title;
+	private Platform platform;
+	
+	//referência a classe Categoria do diagrama de classes - (relação OneToMany)
+	@ManyToOne //chave estrangiera - relacionamento muitos para um
+	@JoinColumn(name = "genre_id") //nome do campo chave-estrangeira no banco de dados
+	private Genre genre;
+	
+	@JsonIgnore //para evitar referência circular
+	@OneToMany(mappedBy = "game")
+	private List<Record> records = new ArrayList<>();
+	
+	public Game() {
+	}
+
+	public Game(Long id, String title, Platform platform, Genre genre) {
+		super();
+		Id = id;
+		this.title = title;
+		this.platform = platform;
+		this.genre = genre;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public List<Record> getRecords() {
+		return records;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+}
